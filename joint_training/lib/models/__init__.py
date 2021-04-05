3dfdd1226549ff75bae5d9928662d085f47031b5
ppf-hhh <62131671+ppf-hhh@users.noreply.github.com>
2021-04-05 22:59:02 +0800

    Add files via upload

--- joint_training/lib/models/__init__.py
+++ joint_training/lib/models/__init__.py
@@ -1,1 +1,14 @@
+# ------------------------------------------------------------------------------
+# Copyright (c) Microsoft
+# Licensed under the MIT License.
+# Written by Ke Sun (sunk@mail.ustc.edu.cn)
+# ------------------------------------------------------------------------------
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import models.seg_hrnet
+import models.seg_hrnet_trans_fcn
+
 
